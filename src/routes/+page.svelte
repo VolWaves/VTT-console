ee894a6d6b5994b5ded22c70254a458b40f3dfb1
xianii <xianyi.xia@outlook.com>
2023-12-13 18:17:39 +0800

    add divider at bottom of nav

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,6 +14,7 @@
 
 <div class="min-w-[680px]">
 	<Nav on:message={onHidNewDataMsg} {dataTypesArrayU} bind:device />
+	<div class="divider mt-1" />
 	<Chart {dataTypesArrayU} bind:device />
 	<div class="divider">控制台</div>
 	<Controller bind:device />
