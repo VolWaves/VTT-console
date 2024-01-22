86a43299714704dde093f39cfd016cee650e5348
xianii <xianyi.xia@outlook.com>
2024-01-22 16:07:54 +0800

    add filter for realtime data

--- src/routes/realtimeData.svelte
+++ src/routes/realtimeData.svelte
@@ -1,13 +1,17 @@
 <script>
 	export let dataTypesArrayU;
+	// 显示过滤
+	const filter = [
+		0x10, 0x11, 0x12, 0x13, 0x14, 0x18, 0xf0, 
+	];
 </script>
 
 <div class="flex-1">
 	<div class="mx-auto min-w-[80%]">
-		<dl class="grid gap-x-2 auto-cols-max text-center lg:grid-cols-6 grid-cols-3">
+		<dl class="grid gap-x-2 auto-cols-max text-center lg:grid-cols-7 grid-cols-3">
 			{#if dataTypesArrayU}
 				{#each dataTypesArrayU as item}
-					{#if item.head != 0x00}
+					{#if filter.includes(item.head)}
 						<div class="flex flex-col gap-y-0">
 							<dt class="lg:text-sm text-xs leading-7 text-gray-600">{item.name}</dt>
 							<dd
