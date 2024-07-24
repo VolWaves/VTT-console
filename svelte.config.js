c3cd7ee2dab433c78465efdefd7a8df6bd2fae37
xianii <xianyi.xia@outlook.com>
2024-07-24 14:30:46 +0800

    another relative base workaround

--- svelte.config.js
+++ svelte.config.js
@@ -15,6 +15,10 @@ const config = {
 			precompress: false,
 			strict: true,
 		}),
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/'+process.env.REPO_NAME : '',
+			relative: true
+		}
 	},
 	preprocess: vitePreprocess()
 };
